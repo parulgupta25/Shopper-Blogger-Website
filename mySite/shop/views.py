aae7cfa25d37d2a3b81ebd5b24485ab2c7d7cca6
parulgupta25 <parulg1999@gmail.com>
2022-05-30 10:18:29 +0530

    new update

--- mySite/shop/views.py
+++ mySite/shop/views.py
@@ -109,6 +109,18 @@ def checkout(request):
 
 @csrf_exempt
 def handlerequest(request):
-    # paytm will send post request here
-    return HttpResponse('done')
-    pass
+    # paytm will send you post request here
+    form = request.POST
+    response_dict = {}
+    for i in form.keys():
+        response_dict[i] = form[i]
+        if i == 'CHECKSUMHASH':
+            checksum = form[i]
+
+    verify = Checksum.verify_checksum(response_dict, MERCHANT_KEY, checksum)
+    if verify:
+        if response_dict['RESPCODE'] == '01':
+            print('order successful')
+        else:
+            print('order was not successful because' + response_dict['RESPMSG'])
+    return render(request, 'shop/paymentstatus.html', {'response': response_dict})
